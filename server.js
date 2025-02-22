c8b9aea7fd7b1774b1cf639f52fa9d93288160b8
Peter Hansson <bo.peter.hansson@gmail.com>
2025-02-22 23:42:23 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Azure! Your app is running.');
+});
+
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+});
